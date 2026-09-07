8dbf4f2205c7940f36aa8b1b2618bceec096cfda
owen maxwell <omaxwell@rrc.ca>
2026-09-06 23:23:23 -0500

    feat: edited script to import employee data from the json file instead of having the data in the script file.

--- apps/frontend/src/employee_list_script.js
+++ apps/frontend/src/employee_list_script.js
@@ -1,69 +1,5 @@
-const employeeData = {
-  "Administration": [
-    "Zoë Robins",
-    "Madeleine Madden"
-  ],
-  "Audit": [
-    "Josha Sadowski",
-    "Kate Fleetwood"
-  ],
-  "Banking Operations": [
-    "Priyanka Bose",
-    "Hammed Animashaun",
-    "Álvaro Morte",
-    "Taylor Napier",
-    "Alan Simmonds"
-  ],
-  "Communications": [
-    "Gil Cardinal",
-    "Richard J. Lewis"
-  ],
-  "Corporate Services": [
-    "Randy Bradshaw",
-    "Tracey Cook",
-    "Lubomir Mykytiuk"
-  ],
-  "Facilities": [
-    "Dakota House",
-    "Lori Lea Okemah",
-    "Renae Morrisseau",
-    "Rick Belcourt"
-  ],
-  "Financial Services": [
-    "Selina Hanusa",
-    "Buffy Gaudry",
-    "Shaneen Ann Fox",
-    "Allan Little",
-    "Danny Rabbit"
-  ],
-  "Human Resources": [
-    "Jesse Ed Azure",
-    "Stacy Da Silva",
-    "Vladimír Valenta",
-    "Samone Sayeses-Whitney",
-    "Paul Coeur"
-  ],
-  "Information Technology": [
-    "Graham Greene",
-    "Sandika Evergreen",
-    "Jennifer Rodriguez (Software Developer)"
-  ],
-  "IT Technician": [
-    "Aiyana Littlebear",
-    "Inara Thunderbird",
-    "Kaya Runningbrook",
-    "Elara Firehawk",
-    "Siona Moonflower",
-    "Kaiyu Greywolf",
-    "Ayawamat Nightwind",
-    "Tala Braveheart",
-    "Iniko Stonebear",
-    "Onatah Redhawk"
-  ]
-}
-
 const employeeList = (listNode, employeeData) => {
-
+    console.log(employeeData)
     Object.entries(employeeData).forEach(([department, employees]) => {
         const departmentLiNode = document.createElement("ul");
         departmentLiNode.innerHTML = `<a>department: ${department}</a>`;
@@ -76,8 +12,24 @@ const employeeList = (listNode, employeeData) => {
     });
 }
 
-document.addEventListener("DOMContentLoaded", () => {
+async function collectEmployeeData() {
+    
     const employeeListNode = document.querySelector(".employee_list");
-    employeeList(employeeListNode, employeeData)
+
+    try {
+        const response = await fetch("./assets/employee_list.json");
+        if (!response.ok) {
+            throw new Error(`Error collecting employee data
+                 Error Status: ${response.status}`);
+        }
+        const employeeData = await response.json();
+        employeeList(employeeListNode, employeeData)
+    } catch (error) {
+    console.error("Error collecting employee data:", error)
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    collectEmployeeData();
 })
 
