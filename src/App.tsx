f206f66b457c1deb19f9657f1f46dd18c7e33960
owen maxwell <omaxwell@rrc.ca>
2026-09-22 18:08:12 -0500

    feat: rollback to last commit to fix broken scripts and start state coding from scratch.

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,25 @@ import EmployeeList from "./components/common/employee_list/employee_list"
 import Header from "./components/common/header/header";
 import Footer from "./components/common/footer/footer";
 import { NewEmployee } from "./components/common/new_employee/new_employee";
-
-
+import { useState } from "react";
+import employeeList from "./assets/employee_list.json"
 
 function App() {
+
+    // Initialize state for employees
+    const [employees, setEmployees] = useState(employeeData);
     return (
         <>
             <Header />
-            <EmployeeList />
-            <NewEmployee />
+            // EmployeeList takes the latest state of employee list.
+            <EmployeeList 
+                employees={employees}
+            />
+            // NewEmployeee takes latest state as well as the function to update that sate.
+            <NewEmployee 
+                employees={employees}
+                setEmployees={setEmployees}
+            />
             <Footer />
         </>
     );
